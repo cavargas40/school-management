5d139d70cf95e10d02bb84ef3216ea497dac8fc6
Carlos Vargas <cavargas40@gmail.com>
2020-07-06 14:16:34 -0500

    create lesson graphql mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,13 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entitiy';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(returns => LessonType)
   lesson() {
     return {
@@ -12,5 +16,14 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  @Mutation(returns => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 }
